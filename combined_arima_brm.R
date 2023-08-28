db7fdcd9d9bdcc0452f335ca9544c268986d8ba0
George Chacko <chackoge@illinois.edu>
2023-08-28 16:05:14 -0500

    added bsts data and updated

--- combined_arima_brm.R
+++ combined_arima_brm.R
@@ -1,5 +1,8 @@
-# script to compare BRM with and without proposal information against
-# ARIMA model for accuracy on Grainger expenditure data
+# Predict Grainger sfy 2023 expenditures based on sfy 2019-2022
+# Models: (i)  BRM exp 
+# (ii) BRM exp + proposal counts 
+# (iii) ARIMA model 
+# (iv) BSTS variants
 
 library(data.table)
 library(lubridate)
@@ -16,6 +19,7 @@ library(bayestestR)
 library(ggeffects)
 library(BayesPostEst)
 library(bayesforecast)
+library(bsts)
 
 rm(list=ls())
 
@@ -85,31 +89,21 @@ merged$cal_monthd <- factor(merged$cal_monthd, levels=c('Jul','Aug','Sep','Oct',
 colnames(merged)[5] <- 'prop_count'
 
 # model building BRM
-bm1 <- stan_glm(imputed_diff2 ~ sfy_year + cal_monthd, data=merged)   
-bm2 <- stan_glm(imputed_diff2 ~ sfy_year + cal_monthd + prop_count, data=merged) 
+exp_model <- stan_glm(imputed_diff2 ~ sfy_year + cal_monthd, data=merged)   
+exp_prop_model <- stan_glm(imputed_diff2 ~ sfy_year + cal_monthd + prop_count, data=merged) 
 
-pp_check(bm1,"dens_overlay")
-pp_check(bm2,"dens_overlay")
+pp_check(exp_model,"dens_overlay")
+pp_check(exp_prop_model,"dens_overlay")
 
-p_direction(bm1)
-p_direction(bm2)
+p_direction(exp_model)
+p_direction(exp_prop_model)
 
 #### 
-# build test set sfy 2023
 
-test <- merged[sfy_year==2023]
-df <- cbind(test[,.(cal_monthd,imputed_diff2)],
-            exp_mdl=apply(posterior_predict(bm1,merged[sfy_year==2023]),2,median),
-            exp_prop_mdl=apply(posterior_predict(bm2,merged[sfy_year==2023]),2,median))
-
-m_df <- reshape2::melt(df,id='cal_monthd')
-ggplot(m_df,aes(x=cal_monthd,y=value,group=variable)) + geom_point(aes(color=variable)) + 
-  geom_line(aes(color=variable)) + theme_bw()
-
-
-
-df[,exp_dev:=100*((imputed_diff2 - exp_mdl)/imputed_diff2)]
-df[,exp_prop_dev:=100*((imputed_diff2 - exp_prop_mdl)/imputed_diff2)]
+# predict and compare to observed values of sfy 2024
+df <- cbind(merged[sfy_year==2023][,.(sfy_year,cal_monthd,imputed_diff2)],
+            exp_mdl=apply(posterior_predict(exp_model,merged[sfy_year==2023][,.(sfy_year,cal_monthd)]),2,median),
+            exp_prop_mdl=apply(posterior_predict(exp_prop_model,merged[sfy_year==2023][,.(sfy_year,cal_monthd,prop_count)]),2,median))
 
 #### model building ARIMA
 # training set sfy 2019-2022
@@ -126,3 +120,41 @@ if(sum(is.na(train_ts)) !=0) {
 est_lambda<- BoxCox.lambda(train_ts)
 arima_model <- auto.arima(train_ts,lambda=est_lambda,approximation=FALSE)
 arima_forecast <- forecast(arima_model, level=c(95),h =1*12)
+
+df2 <- cbind(df,arima=as.vector(arima_forecast$mean))
+
+## BSTS approach...local + seasonal
+ss <- AddLocalLinearTrend(list(), train_ts)
+ss <- AddSeasonal(ss, train_ts, nseasons = 48)
+bsts1 <- bsts(train_ts,
+               state.specification = ss,
+               niter = 1000)
+pred1 <- predict(bsts1, horizon = 12)
+plot(pred1,plot.original=48)
+df3 <- cbind(df2,bsts1=pred1$mean)
+
+# semi-local linear controls uncertainty through stationarity
+ss2 <- AddSemilocalLinearTrend(list(), train_ts)
+ss2 <- AddSeasonal(ss2, train_ts, nseasons = 48)
+bsts2 <- bsts(train_ts, state.specification = ss2, niter = 1000)
+pred2 <- predict(bsts2, horizon = 12)
+
+df4 <- cbind(df3,bsts2=pred2$mean)
+df4[,ex_pd:=100*(imputed_diff2-exp_mdl)/imputed_diff2]
+df4[,ex_pr_pd:=100*(imputed_diff2-exp_prop_mdl)/imputed_diff2]
+df4[,arima_pd:=100*(imputed_diff2-arima)/imputed_diff2]
+df4[,bsts1_pd:=100*(imputed_diff2-bsts1)/imputed_diff2]
+df4[,bsts2_pd:=100*(imputed_diff2-bsts2)/imputed_diff2]
+
+m_df4 <- reshape2::melt(df4,id='cal_monthd')
+ggplot(m_df4[variable %in% c('cal_monthd','imputed_diff2','bsts1','bsts2')],aes(x=cal_monthd,y=value,group=variable)) + 
+  geom_point(aes(color=variable)) + 
+  geom_line(aes(color=variable)) + geom_hline(yintercept =0) + theme_bw()
+
+
+df5 <- df4[,.(cal_monthd,ex_pd,ex_pr_pd,arima_pd,bsts1_pd,bsts2_pd )]
+m_df5 <- reshape2::melt(df5,id='cal_monthd')
+
+ggplot(m_df5,aes(x=cal_monthd,y=value,group=variable)) + geom_point(aes(color=variable)) + 
+  geom_line(aes(color=variable)) + geom_hline(yintercept =0) + theme_bw() + 
+  ylab("Percent Deviation From Observed")
